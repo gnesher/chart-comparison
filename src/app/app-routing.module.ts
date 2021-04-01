950480b79720e9775effe8a7c5d512e1d5637bc2
Guy Nesher <nesher.guy@gmail.com>
2021-04-01 08:20:48 +0300

    initial

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { Ng2Component } from './ng2container/ng2/ng2.component';
+import { NgxComponent } from './ngxchartscontainer/ngx/ngx.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'ng2charts', component: Ng2Component },
+  { path: 'ngxcharts', component: NgxComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,44 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ChartsModule } from 'ng2-charts';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BarChartComponent } from './ng2container/bar-chart/bar-chart.component';
+import { BubbleChartComponent } from './ng2container/bubble-chart/bubble-chart.component';
+import { LineChartComponent } from './ng2container/line-chart/line-chart.component';
+import { PieChartComponent } from './ng2container/pie-chart/pie-chart.component';
+import { RadarChartComponent } from './ng2container/radar-chart/radar-chart.component';
+import { DoughnutChartComponent } from './ng2container/doughnut-chart/doughnut-chart.component';
+import { Ng2Component } from './ng2container/ng2/ng2.component';
+import { NgxComponent } from './ngxchartscontainer/ngx/ngx.component';
+import { NgxChartsModule } from '@swimlane/ngx-charts';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgxlineComponent } from './ngxchartscontainer/ngxline/ngxline.component';
+import {PortalModule} from '@angular/cdk/portal';
+import { NgxbarComponent } from './ngxchartscontainer/ngxbar/ngxbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BarChartComponent,
+    BubbleChartComponent,
+    LineChartComponent,
+    PieChartComponent,
+    RadarChartComponent,
+    DoughnutChartComponent,
+    Ng2Component,
+    NgxComponent,
+    NgxlineComponent,
+    NgxbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ChartsModule,
+    NgxChartsModule,
+    BrowserAnimationsModule,
+    PortalModule
   ],
   providers: [],
   bootstrap: [AppComponent]
